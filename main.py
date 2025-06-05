6379cdcd1a3ff21052118c6d1374556e1c1f8bcf
asza <arek.szatan@interia.pl>
2025-06-05 15:04:43 -0700

    add resources

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from settings import *
 from map import *
 from player import *
 from raycasting import *
+from object_renderer import *
 
 class Game:
     def __init__(self):
-        self.raycasting = None
-        self.player = None
-        self.map = None
         pg.init()
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
@@ -19,6 +17,7 @@ class Game:
     def new_game(self):
         self.map = Map(self)
         self.player = Player(self)
+        self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
 
     def update(self):
--- raycasting.py
+++ raycasting.py
@@ -5,6 +5,9 @@ from settings import *
 class RayCasting:
     def __init__(self, game):
         self.game = game
+        self.ray_casting_result = []
+        self.objects_to_render = []
+        self.textures = self.game.object_renderer.wall_textures
 
     def ray_cast(self):
         ox, oy = self.game.player.pos
@@ -27,6 +30,7 @@ class RayCasting:
             for i in range(MAX_DEPTH):
                 tile_hor = int(x_hor), int(y_hor)
                 if tile_hor in self.game.map.world_map:
+                    texture_hor = self.game.map.world_map[tile_hor]
                     break
                 x_hor += dx
                 y_hor += dy
@@ -44,6 +48,7 @@ class RayCasting:
             for i in range(MAX_DEPTH):
                 tile_vert = int(x_vert), int(y_vert)
                 if tile_vert in self.game.map.world_map:
+                    texture_vert = self.game.map.world_map[tile_vert]
                     break
                 x_vert += dx
                 y_vert += dy
@@ -51,9 +56,13 @@ class RayCasting:
 
             # depth
             if depth_vert < depth_hor:
-                depth = depth_vert
+                depth, texture = depth_vert, texture_vert
+                y_vert %= 1
+                offset = y_vert if cos_a >0 else (1- y_vert)
             else:
-                depth = depth_hor
+                depth, texture = depth_hor, texture_hor
+                x_hor %= 1
+                offset = (1 - x_hor) if sin_a > 0 else x_hor
 
             # remove fishbowl effects
             depth *= math.cos(self.game.player.angle - ray_angle)
